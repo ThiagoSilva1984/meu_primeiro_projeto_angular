afc753e11f1e0c445c1fb0c2487d52244f89dfe3
ThiagoSilva1984 <dasilva.thiagopaulo@gmail.com>
2022-10-01 22:16:26 -0300

    Priemira versão

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+export class FirstComponentComponent implements OnInit {
+  name: string = "Mateus";
+  age: number = 30;
+  job = 'Programador';
+  hobbies = ['correr', 'tennis', 'bike'];
+  car = {
+    name: "Polo",
+    year: 2019,
+  };
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
